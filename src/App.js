2b6296a11b38f40f386d39180223c364437ac169
ghassen mejri <[ghasen29@gmail.com]>
2022-10-09 19:05:20 +0100

    first props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './Profile/Profile';
+
 
 function App() {
+  const handleAlert=(name) =>{
+    alert(`hi my name is ${name}`)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Profile fullName="Elon Reeve Musk FRS" Professions=" Entrepreneur, Investisseur, Homme d'affaires, Producteur de film, Technologue, Inventeur, Dessinateur industriel"
+      bio="entrepreneur américain d'origine sud-africaine qui a cofondé la société de paiement électronique PayPal et créé SpaceX, fabricant de lanceurs et d'engins spatiaux.">
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/SpaceX_CEO_Elon_Musk_visits_N%26NC_and_AFSPC_%28190416-F-ZZ999-006%29_%28cropped%29.jpg/170px-SpaceX_CEO_Elon_Musk_visits_N%26NC_and_AFSPC_%28190416-F-ZZ999-006%29_%28cropped%29.jpg'
+        handleAlert={handleAlert}
+        />
+      </Profile>
     </div>
   );
 }
